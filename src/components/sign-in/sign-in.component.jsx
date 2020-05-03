b52496d47992339ba3f525fa0949be62b5869c39
rozay10 <raadeniyi3@gmail.com>
2020-05-03 07:56:06 +0100

    fnishing signin and submit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInwithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInwithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -15,10 +15,19 @@ export default class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
 
     this.setState({ email: "", password: "" });
+
+      
+    } catch (error) {
+      console.error(error);
+    }
   };
   handleChange = (event) => {
     const { name, value } = event.target;
